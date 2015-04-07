ec591bd3e9bc429957a8ec52b4cfb5f2c51a9da9
Usman Aftab <usman.aftab@gmail.com>
2015-04-07 23:00:13 +0400

    http authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "usman", password: "aftab", except: [:index, :show]
 
     def index
         @articles = Article.all
